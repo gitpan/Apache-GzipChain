af292f15c3df35a4f3f9e592229fba38ff2e2ef8
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
2002-08-26 10:20:41 +0000

    Import of ANDK/Apache-GzipChain-1.13 from CPAN.
    gitpan-cpan-distribution: Apache-GzipChain
    gitpan-cpan-version:      1.13
    gitpan-cpan-path:         ANDK/Apache-GzipChain-1.13.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

--- GzipChain.pm
+++ GzipChain.pm
@@ -2,7 +2,7 @@ package Apache::GzipChain;
 use Compress::Zlib 1.0;
 use strict;
 use vars qw(@ISA $VERSION);
-$VERSION = "0.06";
+$VERSION = sprintf "%d.%02d", q$Revision: 1.13 $ =~ /(\d+).(\d+)/;
 
 use Apache::OutputChain;
 @ISA = qw( Apache::OutputChain );
@@ -10,30 +10,18 @@ use Apache::OutputChain;
 sub handler {
   my($r) = shift;
   my($can_gzip);
-  my @vary = $r->header_out('Vary') if $r->header_out('Vary');
-  push @vary, "Accept-Encoding", "User-Agent";
-  $r->header_out('Vary',
-		 join ", ",
-		 @vary
-		);
-  my($accept_encoding) = $r->header_in("Accept-Encoding");
-  $can_gzip = 1 if index($accept_encoding,"gzip")>=0;
-  unless ($can_gzip) {
-    my $user_agent = $r->header_in("User-Agent");
-    if ($user_agent =~ m{
-			 ^Mozilla/
-			 \d+
-			 \.
-			 \d+
-			 [\s\[\]\w\-]+
-			 (
-			  \(X11 |
-			  Macint.+PPC,\sNav
-			 )
-			}x
-       ) {
-      $can_gzip = 1;
-    }
+  if ($r->dir_config('GzipForce')) {
+    $can_gzip = 1;
+  } else {
+    my @vary;
+    @vary = $r->header_out('Vary') if $r->header_out('Vary');
+    push @vary, "Accept-Encoding";
+    $r->header_out('Vary',
+		   join ", ",
+		   @vary
+		  );
+    my($accept_encoding) = $r->header_in("Accept-Encoding") || "";
+    $can_gzip = 1 if index($accept_encoding,"gzip")>=0;
   }
   if ($can_gzip) {
     $r->header_out('Content-Encoding','gzip');
@@ -45,6 +33,10 @@ sub PRINT {
   my $self = shift;
   my $res = join "", @_;
   return unless length($res);
+  if ($] > 5.007) {
+    require Encode;
+    $res = Encode::encode_utf8($res); # noop if $res doesn't have the UTF-8 flag set
+  }
   $self->Apache::OutputChain::PRINT(Compress::Zlib::memGzip($res));
 }
 
@@ -63,34 +55,19 @@ In the configuration of your apache add something like
 
     <Files *.html>
     SetHandler perl-script
-    PerlHandler Apache::OutputChain Apache::GzipChain Apache::PassFile
+    PerlHandler Apache::OutputChain Apache::GzipChain __your_handler__
+    PerlSetVar GzipForce on  # optional
     </Files>
-
-=head1 STATUS
-
-B<This module is alpha software. Occasional SEGV have been observed.
-Use with caution!>
-
-You currently cannot mix perl's own C<print> statements that print to
-STDOUT and the C<print> or C<write_client> methods in Apache.pm. If
-you do that, you will very likely encounter empty documents and
-probably core dumps too.
 
 =head1 DESCRIPTION
 
-This module compresses any output from another perl handler if and
-only if the browser understands gzip encoding. To determine if the
-browser is able to understand us we check both its I<Accept-Encoding>
-header and its I<User-Agent> header. We check the latter because too
-few browsers send the I<Accept-Encoding> header currently. Instead I
-have set up an enquiry form at
-http://www.kulturbox.de/perl/test/content-encoding-gzip where many
-users already have checked their browsers' abilities and left a
-message. Thus we can test a regular expression against the
-I<User-Agent> header.
+This module compresses any output from another perl handler if the
+browser requesting the document understands gzip encoding or if the
+config variable ForceGzip is set. To determine if the browser is able
+to understand us we check its I<Accept-Encoding> header.
 
-The module seems to work without influencing the other handlers. The
-only thing that can be noticed by the other handler is that the
+The module works without influencing the other handlers in the chain.
+The only thing that can be noticed by other handlers is that the
 response header 'Content-Encoding' has been set. If GzipChain decides
 not to do any compression, it just declines and doesn't even register
 itself for the output chain.
@@ -100,8 +77,23 @@ output chain separately according to the GZIP specification (RFC
 1952). The compression ratio therefore suffers if the other module
 sends its data in very small chunks. It is recommended that you use as
 few print statements as possible in conjunction with the GzipChain.
-The Apache::PassFile module is an example of an efficient file reader
-for this purpose.
+One single print statement is recommended.
+
+=head1 HEADERS
+
+This handler sets the header C<Content-Encoding> to C<gzip> whenever
+it sends gzipped data.
+
+It also sets or appends to the C<Vary> header the values
+C<Accept-Encoding> unless the config variable GzipForce is set, in
+which case the output is the same for all user agents.
+
+=head1 Unicode
+
+Using this module under perl-5.8 or higher is ok for Unicode data.
+UTF-8 data passed to Compress::Zlib::memGzip() are converted to raw
+UTF-8 before compression takes place. Other data are simply passed
+through.
 
 =head1 PREREQUISITES
 
@@ -113,4 +105,3 @@ Andreas Koenig, koenig@kulturbox.de based on code by Jan Pazdziora,
 adelton@fi.muni.cz
 
 =cut
-
---
+++ t/loadme.t
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+
+BEGIN {
+    print "1..1\n";
+}
+use strict;
+warn "This test only tests if the module can be loaded. The author
+has no idea how it could be tested for its functionality. Sorry.\n";
+use Apache::GzipChain;
+
+print "ok 1\n";
+
